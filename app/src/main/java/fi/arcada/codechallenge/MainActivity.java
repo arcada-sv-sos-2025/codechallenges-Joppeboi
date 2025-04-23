6a48b6bc87f98da090b02054855f132ed9383eb4
Johannes Dahlgren <dahlgrjo@arcada.fi>
2025-04-23 19:44:36 +0300

    Projekt 1

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -3,7 +3,9 @@ package fi.arcada.codechallenge;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,7 +15,12 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     TextView myText;
-    Button myButton;
+    TextView currentNums;
+    EditText inputText;
+    Button calcButton;
+    Button addButton;
+
+    ArrayList<Double> nummer = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,28 +28,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myText = findViewById(R.id.text);
-        myText.setText("Hej");
+        inputText = findViewById(R.id.inputText);
+        currentNums = findViewById(R.id.currentNums);
+        calcButton = findViewById(R.id.Calculate);
+        addButton = findViewById(R.id.Add);
 
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = inputText.getText().toString();
 
-        ArrayList<Double> nummer = new ArrayList<Double>();
-        nummer.add(5.5);
-        nummer.add(5.6);
-        nummer.add(5.7);
-        nummer.add(5.8);
+                if (!input.isEmpty()) {
+                    try {
+                        double number = Double.parseDouble(input);
+                        nummer.add(number);
+                        inputText.setText("");
 
-        myButton = findViewById(R.id.button);
+                        StringBuilder currentNumsString = new StringBuilder();
+                        for (double num : nummer) {
+                            currentNumsString.append(num).append("\n");
+                        }
+                        currentNums.setText(currentNumsString.toString());
 
-       myButton.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               calculate(nummer);
-           }
-       });
+                    } catch (NumberFormatException e) {
+                        myText.setText("Please input a valid number!");
+                    }
+                }
+            }
+        });
+
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculate(nummer);
+            }
+        });
     }
 
+
     private void calculate(ArrayList<Double> num) {
-        double mean = Statistics.calcMean(num);
-        myText.setText(Double.toString(mean));
+        double calcAverage = Statistics.calcMean(num);
+        double calcMax = Statistics.calcMax(num);
+        double calcMin = Statistics.calcMin(num);
+        double calcMedian = Statistics.calcMedian(num);
+        double calcMode = Statistics.calcMode(num);
+        double calcStdev = Statistics.calcQR(num);
+        double calcLQ = Statistics.calcLQ(num);
+        double calcUQ = Statistics.calcUQ(num);
+        double calcQR = Statistics.calcQR(num);
+
+
+
+        myText.setText("Average: " + Double.toString(calcAverage) + "\n" +
+                        "Max: " + Double.toString(calcMax) + "\n" +
+                        "Min: " + Double.toString(calcMin) + "\n" +
+                        "Median: " + Double.toString(calcMedian) + "\n" +
+                        "Mode: " + Double.toString(calcMode) + "\n" +
+                        "Standard Dev: " + Double.toString(calcStdev) + "\n" +
+                        "LQ: " + Double.toString(calcLQ) + "\n" +
+                        "UQ: " + Double.toString(calcUQ) + "\n" +
+                        "QR: " + Double.toString(calcQR));
 
     }
 }
--- app/src/main/java/fi/arcada/codechallenge/Statistics.java
+++ app/src/main/java/fi/arcada/codechallenge/Statistics.java
@@ -2,16 +2,129 @@ package fi.arcada.codechallenge;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Statistics {
     public static double calcMean(ArrayList<Double> num) {
 
-        int sum = 0;
+        double sum = 0;
         for (Double x : num) {
             sum += x;
         }
-        double average = (double)sum / num.size();
+        double average = sum / num.size();
 
         return average;
     }
+
+    public static double calcMin(ArrayList<Double> num) {
+        double min = Collections.min(num);
+
+        return min;
+    }
+
+    public static double calcMax(ArrayList<Double> num) {
+        double max = Collections.max(num);
+
+        return max;
+    }
+
+    public static double calcMedian(ArrayList<Double> num) {
+        Collections.sort(num);
+
+        double median;
+        int size = num.size();
+
+        if (size % 2 == 1) {
+            median = num.get(size / 2);
+        } else {
+            double mid1 = num.get(size / 2 - 1);
+            double mid2 = num.get(size / 2);
+
+            median = (mid1 + mid2) / 2;
+        }
+
+        return median;
+    }
+
+    public static double calcMode(ArrayList<Double> num) {
+        Map<Double, Integer> countMap = new HashMap<>();
+
+        for (double numbers : num) {
+            countMap.put(numbers, countMap.getOrDefault(numbers, 0) + 1);
+        }
+
+        Double mode = num.get(0);
+        int maxCount = 0;
+
+        for (Map.Entry<Double, Integer> entry : countMap.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                mode = entry.getKey();
+            }
+        }
+
+        return mode;
+    }
+
+    public static double calcStdev(ArrayList<Double> num) {
+        double sum = 0;
+        for (Double x : num) {
+            sum += x;
+        }
+
+        double average = (double)sum / num.size();
+        double diff = 0;
+
+        for (double numbers : num) {
+            diff += Math.pow(numbers - average, 2);
+        }
+
+        double stDev = Math.sqrt(diff / (num.size() - 1));
+
+        return stDev;
+    }
+
+    public static double calcLQ(ArrayList<Double> num) {
+        Collections.sort(num);
+
+        int size = num.size();
+        double q1;
+
+        if (size % 2 == 0) {
+            q1 = calcMedian(new ArrayList<>(num.subList(0, size / 2)));
+
+        } else {
+            q1 = calcMedian(new ArrayList<>(num.subList(0, size / 2)));
+        }
+
+        return q1;
+    }
+
+    public static double calcUQ(ArrayList<Double> num) {
+        Collections.sort(num);
+
+        int size = num.size();
+        double q3;
+
+        if (size % 2 == 0) {
+            q3 = calcMedian(new ArrayList<>(num.subList(size / 2, size)));
+        } else {
+            q3 = calcMedian(new ArrayList<>(num.subList(size / 2 + 1, size)));
+        }
+
+        return q3;
+    }
+
+    public static double calcQR(ArrayList<Double> num) {
+
+        double q1 = calcLQ(num);
+        double q3 = calcUQ(num);
+
+        double qr = q3-q1;
+
+        return qr;
+    }
 }
+
